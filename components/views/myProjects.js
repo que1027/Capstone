8f27f35b8c1a9630990277cd404294f5928016e7
Quinton Taylor <quintontaylor29@gmail.com>
2025-04-28 19:45:58 -0500

    projects linked

--- components/views/myProjects.js
+++ components/views/myProjects.js
@@ -1551,20 +1551,25 @@ nbsp;
         C++
   </div>
         <hr />
+        <a href=""></a>
           <hr />
   <div id="language2">
         Java
   </div>
         <hr />
+        <a href="https://github.com/que1027/Projects/tree/0bf4ca6a50c68c6d9922680d0b0928ea57216707/UnitConverter">Unit Converter</a>
         <hr />
   <div id="language3">
         Python
   </div>
         <hr />
-
+        <a href="https://github.com/que1027/Projects/blob/0bf4ca6a50c68c6d9922680d0b0928ea57216707/calculator.py">Calculator</a>
         <hr />
   <div id="language4">
         C#
   </div>
+  <hr />
+        <a href=""></a>
+<hr />
 </section>
 `;
